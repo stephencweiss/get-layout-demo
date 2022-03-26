317153042e7be24a5815afdb496d23dd1aa3d02a
Stephen Weiss <stephen.c.weiss@gmail.com>
2022-03-26 08:39:05 -0600

    🚀 initial demonstration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+// import App from "next/app";
+// import SiteLayout from "../components/SiteLayout";
+// import AccountSettingsLayout from "../components/AccountSettingsLayout";
+// import "../css/tailwind.css";
+// import "../styles/theme.scss";
+// import "../styles/reset.scss";
+// import "../styles/globals.scss";
+// import "prismjs/themes/prism-solarizedlight.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import { getLayout as getDefaultLayout } from "../components/SiteLayout";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => getDefaultLayout(page));
+
+  return getLayout(<Component {...pageProps} />);
 }
 
-export default MyApp
+export default MyApp;
